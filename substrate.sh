ea917e492b441ada5e3fcece9271531b6de049a0
Talles Fagundes <makoto.git@gmail.com>
2023-01-09 14:53:30 -0300

    Added substrate installer by shell script

--- substrate.sh
+++ substrate.sh
@@ -1,18 +1,31 @@
 #!/usr/bin/env bash
 
+clear
+
 if [[ -n $1 ]];
 then
+
   if [ $1 == "ubuntu" ] || [ $1 == "pop" ];
   then
     echo "Ubuntu "
-    sudo apt install --assume-yes git clang curl libssl-dev
-  fi
-  elif [[ $1 == "debian" ]];
+    sudo apt install build-essential
+    sudo apt install --assume-yes git clang curl make libssl-dev
+  elif [ $1 == "debian" ];
   then
     echo "Debian Installation."
-  elif [[ $1 == "arch" ] || [ $1 == "manjaro" ]];
+    sudo apt install build-essential
+    sudo apt install --assume-yes git clang curl libssl-dev llvm libudev-dev make protobuf-compiler
+  elif [ $1 == "arch" ] || [ $1 == "manjaro" ];
   then
     echo "Arch Family Installation"
+    pacman -Syu --needed --noconfirm curl git clang make protobuf
   fi
+
+  if [ ! -x "$(command -v rustc)" ];
+  then
+    echo "Rust has not been installed yet"
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain none -y
+  fi
+
   
 fi
