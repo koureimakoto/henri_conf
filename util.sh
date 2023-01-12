37bd4d998d92e9bbbd0e4d3255a561b428219d39
Talles Fagundes <makoto.git@gmail.com>
2023-01-12 18:19:38 -0300

    First commit of my util. This script install all of my script to prepare the OS

---
+++ util.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# exa is a new option to ls
+# duf is a more visual option to df
+# fd-find using fdfind to find
+# bashtop is a fantastic and beatiful option to top and htop
+# bat, using batcat is a beatiful cat version
+
+
+sudo apt install exa duf ncdu fd-find bashtop bat ripgrep
