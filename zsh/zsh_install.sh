803ec49bd94a3277a3b4080ab97a570510366d07
Talles Fagundes <makoto.git@gmail.com>
2023-01-16 21:26:28 -0300

    Add pacman/arch installation for ZSH

--- zsh/zsh_install.sh
+++ zsh/zsh_install.sh
@@ -2,14 +2,32 @@
 
 clear
 
-if [ ! -n $1 ];
+if [[ ! -n $1 ]];
 then
   echo "Invalided arg. Try out: ./zsh_install.sh <distro: ubuntu | pop | arch>"
   exit 1
 fi
 
+function os_name() {  
+  OS=$(echo $1 | tr '[:lower:]' '[:upper:]')
+  echo "Installing ZSH on $OS"
+}
+
 if [ $1 == "ubuntu" ] || [ $1 == "pop" ];
 then
-  sudo apt install zsh
-  chsh -s /bin/zsh
+  os_name $1
+  # sudo apt update
+  # sudo apt install zsh git
+  # sudo chsh -s $(which zsh)
+  # cp .henri_zshrc ~/.zshrc
+elif [ $1 == "arch" ];
+then
+  echo "Installing ZSH on $OS"
+  sudo pacman update
+  sudo pacman -S zsh
+  sudo chsh -s $(which zsh)
+  cp .henri_zshrc ~/.zshrc
 fi
+
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# ln henri.zhs-theme ~/.oh-my-zsh/themes/henri.zsh-theme
